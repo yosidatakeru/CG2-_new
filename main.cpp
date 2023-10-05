f110b8410dac6221e7d6f5bab5a86e5827d545bd
ヨシダタケル <115062870+yosidatakeru@users.noreply.github.com>
2023-10-05 15:41:44 +0900

    Logの表示

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include<Windows.h>
+#include<string>
+#include<format>
+
 
 #pragma region ウィンドウプロシージャ
 //システムメッセージを処理する関数
@@ -20,6 +23,18 @@ LRESULT WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 #pragma endregion
 
 
+#pragma region Logの出力
+void Log(const std::string& message)
+{
+	OutputDebugStringA(message.c_str());
+}
+
+//string->wstring
+std::wstring ConvertString(const std::string& str);
+
+//wstring->string
+std::string ConvertString(const std::wstring& str);
+#pragma endregion
 
 
 
@@ -110,3 +125,40 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 }
 
 
+
+
+#pragma region Logの出力
+std::wstring ConvertString(const std::string& str)
+{
+	if (str.empty())
+	{
+		return std::wstring();
+	}
+
+	auto sizeNeeded = MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), NULL, 0);
+	if (sizeNeeded == 0)
+	{
+		return std::wstring();
+	}
+	std::wstring result(sizeNeeded, 0);
+	MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), &result[0], sizeNeeded);
+	return result;
+}
+
+std::string ConvertString(const std::wstring& str)
+{
+	if (str.empty())
+	{
+		return std::string();
+	}
+
+	auto sizeNeeded = WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), NULL, 0, NULL, NULL);
+	if (sizeNeeded == 0)
+	{
+		return std::string();
+	}
+	std::string result(sizeNeeded, 0);
+	WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), result.data(), sizeNeeded, NULL, NULL);
+	return result;
+}
+#pragma endregion
